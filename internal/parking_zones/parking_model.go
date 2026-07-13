6c82604fc7175378c47a6a0ad30f4dbebeb6535b
Ariful <arifulmit17@gmail.com>
2026-07-14 00:00:05 +0600

    added field in parking model

--- internal/parking_zones/parking_model.go
+++ internal/parking_zones/parking_model.go
@@ -4,4 +4,5 @@ type parking struct {
 	Id             int    `json:"uuid"`
 	Name           string `json:"name"`
 	Total_Capacity int    `json:"total_capacity"`
+	Location       int    `json:"location"`
 }
