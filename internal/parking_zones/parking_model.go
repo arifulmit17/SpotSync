6537e5b423105963ed00d914d8f4c93c7a88fa84
Ariful <arifulmit17@gmail.com>
2026-07-17 23:05:56 +0600

    edited field in parking model

--- internal/parking_zones/parking_model.go
+++ internal/parking_zones/parking_model.go
@@ -1,8 +1,9 @@
 package parking_zones
 
 type parking struct {
-	Id             int    `json:"uuid"`
-	Name           string `json:"name"`
-	Total_Capacity uint   `json:"total_capacity"  gorm:"check:total_capacity > 0"`
-	Location       int    `json:"location"`
+	Id             int     `json:"uuid"`
+	Name           string  `json:"name"`
+	Total_Capacity uint    `json:"total_capacity"  gorm:"check:total_capacity > 0"`
+	Location       int     `json:"location"`
+	Price_per_hour float64 `json:"price_per_hour" gorm:"check:price_per_hour > 0"`
 }
