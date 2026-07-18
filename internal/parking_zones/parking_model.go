6bc8e7f115c46708bfb4e31e2ede718da6a47af8
Ariful <arifulmit17@gmail.com>
2026-07-18 23:52:28 +0600

    added new field

--- internal/parking_zones/parking_model.go
+++ internal/parking_zones/parking_model.go
@@ -6,4 +6,5 @@ type parking struct {
 	Total_Capacity uint    `json:"total_capacity"  gorm:"check:total_capacity > 0"`
 	Location       int     `json:"location"`
 	Price_per_hour float64 `json:"price_per_hour" gorm:"check:price_per_hour > 0"`
+	Created_at     string  `json:"created_at"`
 }
